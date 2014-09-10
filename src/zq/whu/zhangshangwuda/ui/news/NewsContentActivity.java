6ef7f297bd5d0504aea86b60453de5d8acd223d7
eniacluo <eniacsimon@gmail.com>
2014-09-10 15:41:02 +0800

    NewsContentActivity modified1

--- src/zq/whu/zhangshangwuda/ui/news/NewsContentActivity.java
+++ src/zq/whu/zhangshangwuda/ui/news/NewsContentActivity.java
@@ -74,12 +74,12 @@ public class NewsContentActivity extends BaseThemeSwipeBackSherlockActivity {
 				.setShowAsAction(
 						MenuItem.SHOW_AS_ACTION_IF_ROOM
 								| MenuItem.SHOW_AS_ACTION_WITH_TEXT);
-		// menu.add(Menu.NONE, MENU_SNS, 2,
-		// getResources().getString(R.string.share))
-		// .setIcon(R.drawable.ic_menu_share)
-		// .setShowAsAction(
-		// MenuItem.SHOW_AS_ACTION_IF_ROOM
-		// | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+		menu.add(Menu.NONE, MENU_SNS, 2,
+				getResources().getString(R.string.share))
+				.setIcon(R.drawable.ic_menu_share)
+				.setShowAsAction(
+						MenuItem.SHOW_AS_ACTION_IF_ROOM
+								| MenuItem.SHOW_AS_ACTION_WITH_TEXT);
 		;
 		return true;
 	}
@@ -93,10 +93,10 @@ public class NewsContentActivity extends BaseThemeSwipeBackSherlockActivity {
 		case MENU_REFRESH:
 			refreshData();
 			break;
-		// case MENU_SNS:
-		// if (contentmap != null)
-		// sharenews();
-		// break;
+		case MENU_SNS:
+			if (contentmap != null)
+				sharenews();
+			break;
 		}
 		return false;
 	}
@@ -255,17 +255,37 @@ public class NewsContentActivity extends BaseThemeSwipeBackSherlockActivity {
 	// }
 	// };
 
-	// public void sharenews() {
-	// // TODO Auto-generated method stub
-	// String sharecontent = MobclickAgent.getConfigParams(this,
-	// "share_content");
-	// // Map<String, String> sharemap = new HashMap<String, String>();
-	// // sharemap.put("title", contentmap.get("title"));
-	// // sharemap.put("href", contentmap.get("href"));
-	// sharecontent = sharecontent.replace("title", contentmap.get("title"));
-	// sharecontent = sharecontent.replace("href", contentmap.get("href"));
-	// UMSnsService.shareToRenr(NewsContentActivity.this, sharecontent, null);
-	// }
+	 public void sharenews() {
+		 //还没有导sharesdk的包
+		// TODO Auto-generated method stub
+		ShareSDK.initSDK(this);
+		OnekeyShare oks = new OnekeyShare();
+		// 关闭sso授权
+		oks.disableSSOWhenAuthorize();
+
+		// 分享时Notification的图标和文字
+		oks.setNotification(R.drawable.ic_launcher,
+				getString(R.string.app_name));
+		// title标题，印象笔记、邮箱、信息、微信、人人网和QQ空间使用
+		oks.setTitle(getString(R.string.share));
+		// titleUrl是标题的网络链接，仅在人人网和QQ空间使用
+		oks.setTitleUrl("http://sharesdk.cn");
+		// text是分享文本，所有平台都需要这个字段
+		oks.setText("我是分享文本");
+		// imagePath是图片的本地路径，Linked-In以外的平台都支持此参数
+		oks.setImagePath("/sdcard/test.jpg");
+		// url仅在微信（包括好友和朋友圈）中使用
+		oks.setUrl("http://sharesdk.cn");
+		// comment是我对这条分享的评论，仅在人人网和QQ空间使用
+		oks.setComment("我是测试评论文本");
+		// site是分享此内容的网站名称，仅在QQ空间使用
+		oks.setSite(getString(R.string.app_name));
+		// siteUrl是分享此内容的网站地址，仅在QQ空间使用
+		oks.setSiteUrl("http://sharesdk.cn");
+
+		// 启动分享GUI
+		oks.show(this);
+	}
 
 	// public void saveData() {
 	// News tnews = new News(href, title, time, content);
